baf3d3f4855a9a017bfc3279fd00889dee7215b0
flutterkat <85975055+flutterkat@users.noreply.github.com>
2021-07-13 18:36:20 -0400

    2nd draft

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
-const search = new Filter('search', 'data-caption');
-
 baguetteBox.run('.gallery');
+
+document.getElementById("search").addEventListener("keyup", searchFunction);
+
+function searchFunction() {
+  var input, filter, div, a, i, caption;
+  input = document.getElementById("search");
+  filter = input.value.toLowerCase();
+  div = document.getElementsByTagName("gallery");
+  caption = document.getElementsByTagName('a');
+
+for (i=0; i<=11; i++) {
+    a = caption[i].getAttribute('data-caption');
+    if (a.toLowerCase().indexOf(filter) > -1) {
+        document.getElementsByTagName('img')[i].style.display = "";
+    } else {
+        document.getElementsByTagName('img')[i].style.display = "none";
+        }
+    }
+}
